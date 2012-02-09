aedc08079458ac2c9bdd832a5a6ece37a626b186
yukihico <yukihico@gmail.com>
2012-02-09 15:06:18 +0900

    bugfix

--- app/controllers/bizhour_controller.rb
+++ app/controllers/bizhour_controller.rb
@@ -8,7 +8,7 @@ class BizhourController < ApplicationController
 end
 
 class BizHour
-  DH = %w[SUN MON TUE WED THU FRI]
+  DH = %w[SUN MON TUE WED THU FRI SAT]
   def parse(str)
     if(str =~ /(\d{1,2}):(\d{1,2}).*?(\d{1,2}):(\d{1,2})/)
       setRange($1, $2, $3, $4)
